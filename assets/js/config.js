abe766815d52bab1b23e129ba67f404e714b2dae
raavenkkj <grupodoruim@gmail.com>
2023-02-12 20:10:29 -0300

    Initial commit

---
+++ assets/js/config.js
@@ -0,0 +1,27 @@
+const BADGES = [
+  {
+    name: "HypeSquad Bravery", // Message that will appear when hovering over the badge
+    image: "assets/badges/hypesquad_bravery.svg",
+  },
+];
+
+const USER_ID = "563184210387402803"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
+const BACKGROUND_IMAGE = "assets/images/star.gif"; // Here is the background of the site, behind your profile
+const STATUS = {
+  dnd: {
+    name: "Do not disturb", // Discord Status
+    color: "#ed4245",
+  },
+  idle: {
+    name: "Busy",
+    color: "#faa81a",
+  },
+  online: {
+    name: "Online",
+    color: "#3ba55d",
+  },
+  offline: {
+    name: "Offline",
+    color: "#6e7985",
+  },
+};
